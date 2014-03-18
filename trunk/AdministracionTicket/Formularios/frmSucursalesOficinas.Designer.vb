495b3076ec3bebf577bf7c9d9089117f83a1b511
rpach17@gmail.com <rpach17@gmail.com@ecf54b40-d846-b93a-3702-4cecb57a2510>
2014-03-18 17:04:59 +0000

    Nuevo formulario Sucursales oficinas

--- trunk/AdministracionTicket/Formularios/frmSucursalesOficinas.Designer.vb
+++ trunk/AdministracionTicket/Formularios/frmSucursalesOficinas.Designer.vb
@@ -22,76 +22,181 @@ Partial Class frmSucursalesOficinas
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.dgvSucursales = New System.Windows.Forms.DataGridView()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSucursalesOficinas))
+        Me.lblAccion = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.txtBuscarSucursal = New System.Windows.Forms.TextBox()
-        Me.dgvOficinas = New System.Windows.Forms.DataGridView()
-        Me.cboOficicinas = New System.Windows.Forms.ComboBox()
+        Me.dgvSucursales = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnQuitar = New System.Windows.Forms.Button()
         Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.cboOficicinas = New System.Windows.Forms.ComboBox()
+        Me.dgvOficinas = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
         CType(Me.dgvSucursales, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
         CType(Me.dgvOficinas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'dgvSucursales
+        'lblAccion
         '
-        Me.dgvSucursales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvSucursales.Location = New System.Drawing.Point(12, 35)
-        Me.dgvSucursales.Name = "dgvSucursales"
-        Me.dgvSucursales.Size = New System.Drawing.Size(240, 282)
-        Me.dgvSucursales.TabIndex = 0
+        Me.lblAccion.BackColor = System.Drawing.Color.Gray
+        Me.lblAccion.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lblAccion.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.lblAccion.ForeColor = System.Drawing.Color.White
+        Me.lblAccion.Location = New System.Drawing.Point(0, 0)
+        Me.lblAccion.Name = "lblAccion"
+        Me.lblAccion.Size = New System.Drawing.Size(722, 44)
+        Me.lblAccion.TabIndex = 14
+        Me.lblAccion.Text = "OFICINAS POR SUCURSAL"
+        Me.lblAccion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.txtBuscarSucursal)
+        Me.Panel1.Controls.Add(Me.dgvSucursales)
+        Me.Panel1.Location = New System.Drawing.Point(7, 47)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(383, 365)
+        Me.Panel1.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Image = CType(resources.GetObject("Label1.Image"), System.Drawing.Image)
+        Me.Label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Label1.Location = New System.Drawing.Point(12, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 33)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Buscar Sucursal"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'txtBuscarSucursal
         '
-        Me.txtBuscarSucursal.Location = New System.Drawing.Point(12, 9)
+        Me.txtBuscarSucursal.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtBuscarSucursal.Location = New System.Drawing.Point(170, 47)
         Me.txtBuscarSucursal.Name = "txtBuscarSucursal"
-        Me.txtBuscarSucursal.Size = New System.Drawing.Size(240, 20)
-        Me.txtBuscarSucursal.TabIndex = 1
+        Me.txtBuscarSucursal.Size = New System.Drawing.Size(203, 20)
+        Me.txtBuscarSucursal.TabIndex = 6
         '
-        'dgvOficinas
+        'dgvSucursales
         '
-        Me.dgvOficinas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvOficinas.Location = New System.Drawing.Point(258, 79)
-        Me.dgvOficinas.Name = "dgvOficinas"
-        Me.dgvOficinas.Size = New System.Drawing.Size(240, 238)
-        Me.dgvOficinas.TabIndex = 2
+        Me.dgvSucursales.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvSucursales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSucursales.Location = New System.Drawing.Point(12, 79)
+        Me.dgvSucursales.Name = "dgvSucursales"
+        Me.dgvSucursales.Size = New System.Drawing.Size(361, 282)
+        Me.dgvSucursales.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Gray
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(383, 40)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Sucursales"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.btnQuitar)
+        Me.Panel2.Controls.Add(Me.btnAgregar)
+        Me.Panel2.Controls.Add(Me.cboOficicinas)
+        Me.Panel2.Controls.Add(Me.dgvOficinas)
+        Me.Panel2.Location = New System.Drawing.Point(396, 47)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(319, 365)
+        Me.Panel2.TabIndex = 16
+        '
+        'btnQuitar
+        '
+        Me.btnQuitar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuitar.Location = New System.Drawing.Point(38, 70)
+        Me.btnQuitar.Name = "btnQuitar"
+        Me.btnQuitar.Size = New System.Drawing.Size(118, 38)
+        Me.btnQuitar.TabIndex = 9
+        Me.btnQuitar.Text = "Quitar"
+        Me.btnQuitar.UseVisualStyleBackColor = True
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAgregar.Location = New System.Drawing.Point(160, 70)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(118, 38)
+        Me.btnAgregar.TabIndex = 7
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
         '
         'cboOficicinas
         '
         Me.cboOficicinas.FormattingEnabled = True
-        Me.cboOficicinas.Location = New System.Drawing.Point(258, 8)
+        Me.cboOficicinas.Location = New System.Drawing.Point(38, 44)
         Me.cboOficicinas.Name = "cboOficicinas"
         Me.cboOficicinas.Size = New System.Drawing.Size(240, 21)
-        Me.cboOficicinas.TabIndex = 3
+        Me.cboOficicinas.TabIndex = 6
         '
-        'btnAgregar
+        'dgvOficinas
         '
-        Me.btnAgregar.Location = New System.Drawing.Point(380, 35)
-        Me.btnAgregar.Name = "btnAgregar"
-        Me.btnAgregar.Size = New System.Drawing.Size(118, 38)
-        Me.btnAgregar.TabIndex = 4
-        Me.btnAgregar.Text = "Agregar"
-        Me.btnAgregar.UseVisualStyleBackColor = True
+        Me.dgvOficinas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvOficinas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOficinas.Location = New System.Drawing.Point(38, 114)
+        Me.dgvOficinas.Name = "dgvOficinas"
+        Me.dgvOficinas.Size = New System.Drawing.Size(240, 243)
+        Me.dgvOficinas.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Gray
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(0, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(319, 40)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "Oficinas"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmSucursalesOficinas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(515, 329)
-        Me.Controls.Add(Me.btnAgregar)
-        Me.Controls.Add(Me.cboOficicinas)
-        Me.Controls.Add(Me.dgvOficinas)
-        Me.Controls.Add(Me.txtBuscarSucursal)
-        Me.Controls.Add(Me.dgvSucursales)
+        Me.ClientSize = New System.Drawing.Size(722, 414)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblAccion)
         Me.Name = "frmSucursalesOficinas"
-        Me.Text = "frmSucursalesOficinas"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         CType(Me.dgvSucursales, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
         CType(Me.dgvOficinas, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents dgvSucursales As System.Windows.Forms.DataGridView
+    Friend WithEvents lblAccion As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents txtBuscarSucursal As System.Windows.Forms.TextBox
-    Friend WithEvents dgvOficinas As System.Windows.Forms.DataGridView
-    Friend WithEvents cboOficicinas As System.Windows.Forms.ComboBox
+    Friend WithEvents dgvSucursales As System.Windows.Forms.DataGridView
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnQuitar As System.Windows.Forms.Button
     Friend WithEvents btnAgregar As System.Windows.Forms.Button
+    Friend WithEvents cboOficicinas As System.Windows.Forms.ComboBox
+    Friend WithEvents dgvOficinas As System.Windows.Forms.DataGridView
 End Class
