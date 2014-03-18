735fd324fe5bb3160e612fa90c23bd1a1e4d4f3e
rpach17 <rpach17@gmail.com>
2014-03-18 17:37:16 +0000

    nose

--- AdministracionTicket/My Project/Application.Designer.vb
+++ AdministracionTicket/My Project/Application.Designer.vb
@@ -33,6 +33,7 @@ Namespace My
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.AdministracionTicket.frmGestionesOficina
+            Me.MainForm = Global.AdministracionTicket.frmSucursalesOficinas
         End Sub
     End Class
 End Namespace
