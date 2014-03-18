13daf7ac58e2ca7c0799c74e3728a0ea2609b982
josuetec2003 <josuetec2003@gmail.com>
2014-03-18 17:03:44 +0000

    Se agrega formulari para administrar las gestiones por oficina

--- AdministracionTicket/Complementos/ModuloProcedimientos.vb
+++ AdministracionTicket/Complementos/ModuloProcedimientos.vb
@@ -8,6 +8,12 @@
             ElseIf TypeOf (ctrl) Is CheckBox Then
                 Dim chkCtrl As CheckBox = DirectCast(ctrl, CheckBox)
                 chkCtrl.Checked = False
+            ElseIf TypeOf (ctrl) Is ComboBox Then
+                Dim cbo As ComboBox = DirectCast(ctrl, ComboBox)
+                cbo.SelectedValue = -1
+            ElseIf TypeOf (ctrl) Is NumericUpDown Then
+                Dim nud As NumericUpDown = DirectCast(ctrl, NumericUpDown)
+                nud.Value = 0
             End If
         Next
     End Sub
---
+++ AdministracionTicket/Formularios/frmGestionesOficina.vb
@@ -0,0 +1,74 @@
+﻿Public Class frmGestionesOficina
+    Dim _idgestion As Integer
+
+    Private Sub frmGestionesOficina_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        EntityTablas.CargarOficinas(cboOficina)
+    End Sub
+
+    Private Function ValidarForm() As Boolean
+        If txtCodigo.Text.Trim = "" Then
+            MsgBox("Debe ingresar el código de la gestión", MsgBoxStyle.Exclamation, "Código")
+            txtCodigo.Focus()
+            Return False
+        End If
+
+        If txtGestion.Text.Trim = "" Then
+            MsgBox("Debe ingresar el nombre de la gestión", MsgBoxStyle.Exclamation, "Nombre")
+            txtGestion.Focus()
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub btnAgregar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregar.Click
+        If Not ValidarForm() Then
+            Exit Sub
+        End If
+
+        If btnAgregar.Text = "Agregar gestión" Then
+            Dim ID As Integer = EntityTablas.AgregarGestion(New GESTIONES() With _
+           {
+               .CODIGO = txtCodigo.Text, _
+               .NOMBRE = txtGestion.Text, _
+               .TIEMPO = txtTiempo.Value _
+           })
+
+            EntityTablas.AgregarOFGE(New DETALLE_OFICINA_GESTIONES() With _
+            {
+                .IDOFICINA = cboOficina.SelectedValue, _
+                .IDGESTION = ID _
+            })
+
+            EntityTablas.CargarGestionesXOficinas(dgvGestionesXOficina, cboOficina.SelectedValue)
+            LimpiarControles(txtCodigo, txtGestion, txtTiempo)
+            txtCodigo.Focus()
+        Else
+            EntityTablas.ActualizarGestion(_idgestion, txtCodigo.Text, txtGestion.Text, txtTiempo.Value)
+            EntityTablas.CargarGestionesXOficinas(dgvGestionesXOficina, cboOficina.SelectedValue)
+            LimpiarControles(txtCodigo, txtGestion, txtTiempo)
+            MsgBox("Los datos de la gestión se actualizaron con éxito", MsgBoxStyle.Information, "Gestión")
+            btnAgregar.Text = "Agregar gestión"
+        End If
+    End Sub
+
+    Private Sub cboOficina_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboOficina.SelectedIndexChanged
+        Try
+            EntityTablas.CargarGestionesXOficinas(dgvGestionesXOficina, cboOficina.SelectedValue)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub dgvGestionesXOficina_DoubleClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles dgvGestionesXOficina.DoubleClick
+        _idgestion = ObtenerDatoGrid(dgvGestionesXOficina)
+        Dim _codigo As String = ObtenerDatoGrid(dgvGestionesXOficina, 1)
+        Dim _gestion As String = ObtenerDatoGrid(dgvGestionesXOficina, 2)
+        Dim _tiempo As Integer = ObtenerDatoGrid(dgvGestionesXOficina, 3)
+
+        txtCodigo.Text = _codigo
+        txtGestion.Text = _gestion
+        txtTiempo.Value = _tiempo
+
+        btnAgregar.Text = "Actualizar gestión"
+    End Sub
+End Class
