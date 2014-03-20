fdbb9b45c2f136210e8ec2f5d6ac4adb50dab42e
rpach17@gmail.com <rpach17@gmail.com@ecf54b40-d846-b93a-3702-4cecb57a2510>
2014-03-20 16:22:03 +0000

    Formulario de configuracion

--- trunk/AdministracionTicket/Formularios/frmConfig.Designer.vb
+++ trunk/AdministracionTicket/Formularios/frmConfig.Designer.vb
@@ -22,8 +22,113 @@ Partial Class frmConfig
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.txtDS = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnProbar = New System.Windows.Forms.Button()
+        Me.txtUserID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(216, 64)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(130, 23)
+        Me.btnGuardar.TabIndex = 28
+        Me.btnGuardar.Tag = "4"
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(90, 40)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(100, 20)
+        Me.txtPass.TabIndex = 23
+        Me.txtPass.Tag = "2"
+        '
+        'txtDS
+        '
+        Me.txtDS.Location = New System.Drawing.Point(90, 66)
+        Me.txtDS.Name = "txtDS"
+        Me.txtDS.Size = New System.Drawing.Size(100, 20)
+        Me.txtDS.TabIndex = 24
+        Me.txtDS.Tag = "3"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 66)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 13)
+        Me.Label3.TabIndex = 26
+        Me.Label3.Text = "Data Source"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "Password"
+        '
+        'btnProbar
+        '
+        Me.btnProbar.Location = New System.Drawing.Point(216, 17)
+        Me.btnProbar.Name = "btnProbar"
+        Me.btnProbar.Size = New System.Drawing.Size(130, 23)
+        Me.btnProbar.TabIndex = 27
+        Me.btnProbar.Tag = "4"
+        Me.btnProbar.Text = "Probar Conexion"
+        Me.btnProbar.UseVisualStyleBackColor = True
+        '
+        'txtUserID
+        '
+        Me.txtUserID.Location = New System.Drawing.Point(90, 14)
+        Me.txtUserID.Name = "txtUserID"
+        Me.txtUserID.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserID.TabIndex = 22
+        Me.txtUserID.Tag = "1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "User ID"
+        '
+        'frmConfig
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 118)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtDS)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnProbar)
+        Me.Controls.Add(Me.txtUserID)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmConfig"
         Me.Text = "frmConfig"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents btnGuardar As System.Windows.Forms.Button
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents txtDS As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnProbar As System.Windows.Forms.Button
+    Friend WithEvents txtUserID As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
