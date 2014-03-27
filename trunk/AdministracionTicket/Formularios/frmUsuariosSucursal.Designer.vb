5c847aab62b0d5004a3512916b1e16180878a2de
rpach17@gmail.com <rpach17@gmail.com@ecf54b40-d846-b93a-3702-4cecb57a2510>
2014-03-27 21:30:33 +0000

    Usuario busqueda

--- trunk/AdministracionTicket/Formularios/frmUsuariosSucursal.Designer.vb
+++ trunk/AdministracionTicket/Formularios/frmUsuariosSucursal.Designer.vb
@@ -22,8 +22,62 @@ Partial Class frmUsuariosSucursal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvUsuarios = New System.Windows.Forms.DataGridView()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        CType(Me.dgvUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label1.Location = New System.Drawing.Point(64, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(203, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Buscar por usuario y/o nombre"
+        '
+        'dgvUsuarios
+        '
+        Me.dgvUsuarios.AllowUserToAddRows = False
+        Me.dgvUsuarios.AllowUserToDeleteRows = False
+        Me.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvUsuarios.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvUsuarios.Location = New System.Drawing.Point(63, 62)
+        Me.dgvUsuarios.Name = "dgvUsuarios"
+        Me.dgvUsuarios.ReadOnly = True
+        Me.dgvUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvUsuarios.Size = New System.Drawing.Size(594, 359)
+        Me.dgvUsuarios.TabIndex = 4
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!)
+        Me.txtBuscar.ForeColor = System.Drawing.Color.Black
+        Me.txtBuscar.Location = New System.Drawing.Point(273, 26)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(384, 27)
+        Me.txtBuscar.TabIndex = 3
+        '
+        'frmUsuariosSucursal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(721, 447)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvUsuarios)
+        Me.Controls.Add(Me.txtBuscar)
+        Me.Name = "frmUsuariosSucursal"
         Me.Text = "frmUsuariosSucursal"
+        CType(Me.dgvUsuarios, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents dgvUsuarios As System.Windows.Forms.DataGridView
+    Friend WithEvents txtBuscar As System.Windows.Forms.TextBox
 End Class
