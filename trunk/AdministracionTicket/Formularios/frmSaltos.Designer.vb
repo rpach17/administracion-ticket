ca41ce79ddbd3a299c893708b7b027354bc83aea
josuetec2003@gmail.com <josuetec2003@gmail.com@ecf54b40-d846-b93a-3702-4cecb57a2510>
2014-05-02 19:04:48 +0000

    Formularios de Saltos y Puestos

--- trunk/AdministracionTicket/Formularios/frmSaltos.Designer.vb
+++ trunk/AdministracionTicket/Formularios/frmSaltos.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmOficinas
+Partial Class frmSaltos
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,7 @@ Partial Class frmOficinas
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmOficinas))
-        Me.dgvOficinas = New System.Windows.Forms.DataGridView()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSaltos))
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.Nuevo = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
@@ -35,24 +33,8 @@ Partial Class frmOficinas
         Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
         Me.Eliminar = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
-        Me.txtOficina = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.PuestosDeEstaOficinaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        CType(Me.dgvOficinas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.ToolStrip1.SuspendLayout()
-        Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'dgvOficinas
-        '
-        Me.dgvOficinas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
-        Me.dgvOficinas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvOficinas.ContextMenuStrip = Me.ContextMenuStrip1
-        Me.dgvOficinas.Location = New System.Drawing.Point(12, 83)
-        Me.dgvOficinas.Name = "dgvOficinas"
-        Me.dgvOficinas.Size = New System.Drawing.Size(416, 277)
-        Me.dgvOficinas.TabIndex = 0
         '
         'ToolStrip1
         '
@@ -61,8 +43,8 @@ Partial Class frmOficinas
         Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Nuevo, Me.ToolStripSeparator1, Me.Actualizar, Me.ToolStripSeparator2, Me.Guardar, Me.Cancelar, Me.ToolStripSeparator3, Me.Eliminar, Me.ToolStripSeparator4})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(441, 48)
-        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Size = New System.Drawing.Size(794, 48)
+        Me.ToolStrip1.TabIndex = 2
         Me.ToolStrip1.Text = "ToolStrip1"
         '
         'Nuevo
@@ -130,71 +112,27 @@ Partial Class frmOficinas
         Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
         Me.ToolStripSeparator4.Size = New System.Drawing.Size(6, 48)
         '
-        'txtOficina
-        '
-        Me.txtOficina.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
-        Me.txtOficina.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtOficina.Location = New System.Drawing.Point(87, 51)
-        Me.txtOficina.Name = "txtOficina"
-        Me.txtOficina.Size = New System.Drawing.Size(240, 26)
-        Me.txtOficina.TabIndex = 2
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 54)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(69, 20)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Nombre:"
-        '
-        'ContextMenuStrip1
-        '
-        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PuestosDeEstaOficinaToolStripMenuItem})
-        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(195, 48)
-        '
-        'PuestosDeEstaOficinaToolStripMenuItem
-        '
-        Me.PuestosDeEstaOficinaToolStripMenuItem.Image = Global.AdministracionTicket.My.Resources.Resources.text_list_bullets
-        Me.PuestosDeEstaOficinaToolStripMenuItem.Name = "PuestosDeEstaOficinaToolStripMenuItem"
-        Me.PuestosDeEstaOficinaToolStripMenuItem.Size = New System.Drawing.Size(194, 22)
-        Me.PuestosDeEstaOficinaToolStripMenuItem.Text = "Puestos de esta oficina"
-        '
-        'frmOficinas
+        'frmSaltos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(441, 372)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txtOficina)
+        Me.ClientSize = New System.Drawing.Size(794, 395)
         Me.Controls.Add(Me.ToolStrip1)
-        Me.Controls.Add(Me.dgvOficinas)
-        Me.Name = "frmOficinas"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Oficinas"
-        CType(Me.dgvOficinas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Name = "frmSaltos"
+        Me.Text = "Pasos de la gestión"
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
-        Me.ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents dgvOficinas As System.Windows.Forms.DataGridView
     Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
     Friend WithEvents Nuevo As System.Windows.Forms.ToolStripButton
-    Friend WithEvents Guardar As System.Windows.Forms.ToolStripButton
-    Friend WithEvents txtOficina As System.Windows.Forms.TextBox
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Eliminar As System.Windows.Forms.ToolStripButton
     Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
-    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
     Friend WithEvents Actualizar As System.Windows.Forms.ToolStripButton
-    Friend WithEvents ToolStripSeparator3 As System.Windows.Forms.ToolStripSeparator
-    Friend WithEvents ToolStripSeparator4 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents Guardar As System.Windows.Forms.ToolStripButton
     Friend WithEvents Cancelar As System.Windows.Forms.ToolStripButton
-    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
-    Friend WithEvents PuestosDeEstaOficinaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator3 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents Eliminar As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripSeparator4 As System.Windows.Forms.ToolStripSeparator
 End Class
---
+++ trunk/AdministracionTicket/Formularios/frmSaltos.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmSaltos
+
+End Class
