8512339d114cfd008e7dc5cab6564eed575fd880
josuetec2003@gmail.com <josuetec2003@gmail.com@ecf54b40-d846-b93a-3702-4cecb57a2510>
2014-05-09 16:37:15 +0000

    Algo de diseño

--- trunk/AdministracionTicket/Formularios/frmPrincipal.Designer.vb
+++ trunk/AdministracionTicket/Formularios/frmPrincipal.Designer.vb
@@ -29,11 +29,9 @@ Partial Class frmPrincipal
         Me.lblInfoUbicacion = New System.Windows.Forms.ToolStripStatusLabel()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.OpcionesToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
-        Me.OpcionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.UsuariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ListaDeUsuariosToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.CerrarSesiónToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpcionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SucursalesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NuevaSucursalToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.CatalogoDeSucursalesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -41,17 +39,20 @@ Partial Class frmPrincipal
         Me.AsignarOficinasASucursalToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GestionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.VentanillasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UsuariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ListaDeUsuariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListaDeUsuariosToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.StatusStrip1.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'StatusStrip1
         '
+        Me.StatusStrip1.BackColor = System.Drawing.SystemColors.Control
         Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.lblInfoConexion, Me.ToolStripStatusLabel2, Me.lblInfoUbicacion})
         Me.StatusStrip1.Location = New System.Drawing.Point(0, 368)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(1048, 22)
+        Me.StatusStrip1.Size = New System.Drawing.Size(735, 22)
         Me.StatusStrip1.TabIndex = 5
         Me.StatusStrip1.Text = "StatusStrip1"
         '
@@ -82,10 +83,12 @@ Partial Class frmPrincipal
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.AutoSize = False
+        Me.MenuStrip1.Font = New System.Drawing.Font("Segoe UI", 10.0!)
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpcionesToolStripMenuItem1, Me.OpcionesToolStripMenuItem, Me.UsuariosToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1048, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(735, 47)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -93,29 +96,8 @@ Partial Class frmPrincipal
         '
         Me.OpcionesToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CerrarSesiónToolStripMenuItem, Me.SalirToolStripMenuItem})
         Me.OpcionesToolStripMenuItem1.Name = "OpcionesToolStripMenuItem1"
-        Me.OpcionesToolStripMenuItem1.Size = New System.Drawing.Size(69, 20)
-        Me.OpcionesToolStripMenuItem1.Text = "Opciones"
-        '
-        'OpcionesToolStripMenuItem
-        '
-        Me.OpcionesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SucursalesToolStripMenuItem, Me.GestionesToolStripMenuItem, Me.VentanillasToolStripMenuItem})
-        Me.OpcionesToolStripMenuItem.Name = "OpcionesToolStripMenuItem"
-        Me.OpcionesToolStripMenuItem.Size = New System.Drawing.Size(100, 20)
-        Me.OpcionesToolStripMenuItem.Text = "Administracion"
-        '
-        'UsuariosToolStripMenuItem
-        '
-        Me.UsuariosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ListaDeUsuariosToolStripMenuItem, Me.ListaDeUsuariosToolStripMenuItem1})
-        Me.UsuariosToolStripMenuItem.Name = "UsuariosToolStripMenuItem"
-        Me.UsuariosToolStripMenuItem.Size = New System.Drawing.Size(64, 20)
-        Me.UsuariosToolStripMenuItem.Text = "Usuarios"
-        '
-        'ListaDeUsuariosToolStripMenuItem1
-        '
-        Me.ListaDeUsuariosToolStripMenuItem1.Image = Global.AdministracionTicket.My.Resources.Resources.user
-        Me.ListaDeUsuariosToolStripMenuItem1.Name = "ListaDeUsuariosToolStripMenuItem1"
-        Me.ListaDeUsuariosToolStripMenuItem1.Size = New System.Drawing.Size(162, 22)
-        Me.ListaDeUsuariosToolStripMenuItem1.Text = "Lista de usuarios"
+        Me.OpcionesToolStripMenuItem1.Size = New System.Drawing.Size(69, 43)
+        Me.OpcionesToolStripMenuItem1.Text = "&Opciones"
         '
         'CerrarSesiónToolStripMenuItem
         '
@@ -130,6 +112,13 @@ Partial Class frmPrincipal
         Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
         Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(142, 22)
         Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'OpcionesToolStripMenuItem
+        '
+        Me.OpcionesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SucursalesToolStripMenuItem, Me.GestionesToolStripMenuItem, Me.VentanillasToolStripMenuItem})
+        Me.OpcionesToolStripMenuItem.Name = "OpcionesToolStripMenuItem"
+        Me.OpcionesToolStripMenuItem.Size = New System.Drawing.Size(112, 43)
+        Me.OpcionesToolStripMenuItem.Text = "&Administración"
         '
         'SucursalesToolStripMenuItem
         '
@@ -181,6 +170,13 @@ Partial Class frmPrincipal
         Me.VentanillasToolStripMenuItem.Size = New System.Drawing.Size(131, 22)
         Me.VentanillasToolStripMenuItem.Text = "Ventanillas"
         '
+        'UsuariosToolStripMenuItem
+        '
+        Me.UsuariosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ListaDeUsuariosToolStripMenuItem, Me.ListaDeUsuariosToolStripMenuItem1})
+        Me.UsuariosToolStripMenuItem.Name = "UsuariosToolStripMenuItem"
+        Me.UsuariosToolStripMenuItem.Size = New System.Drawing.Size(64, 43)
+        Me.UsuariosToolStripMenuItem.Text = "&Usuarios"
+        '
         'ListaDeUsuariosToolStripMenuItem
         '
         Me.ListaDeUsuariosToolStripMenuItem.Image = Global.AdministracionTicket.My.Resources.Resources.magnifier
@@ -188,16 +184,26 @@ Partial Class frmPrincipal
         Me.ListaDeUsuariosToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
         Me.ListaDeUsuariosToolStripMenuItem.Text = "Búsqueda rápida"
         '
+        'ListaDeUsuariosToolStripMenuItem1
+        '
+        Me.ListaDeUsuariosToolStripMenuItem1.Image = Global.AdministracionTicket.My.Resources.Resources.user
+        Me.ListaDeUsuariosToolStripMenuItem1.Name = "ListaDeUsuariosToolStripMenuItem1"
+        Me.ListaDeUsuariosToolStripMenuItem1.Size = New System.Drawing.Size(162, 22)
+        Me.ListaDeUsuariosToolStripMenuItem1.Text = "Lista de usuarios"
+        '
         'frmPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1048, 390)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(184, Byte), Integer), CType(CType(252, Byte), Integer), CType(CType(201, Byte), Integer))
+        Me.BackgroundImage = Global.AdministracionTicket.My.Resources.Resources.rnp
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(735, 390)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.IsMdiContainer = True
         Me.Name = "frmPrincipal"
-        Me.Text = "Administracion de Ticket"
+        Me.Text = "Administración de Ticket y Gestiones"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.StatusStrip1.ResumeLayout(False)
         Me.StatusStrip1.PerformLayout()
