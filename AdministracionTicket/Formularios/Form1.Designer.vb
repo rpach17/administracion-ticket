3efa3e682b81ba5790fed7e57ba4625891e5d8dd
josuetec2003 <josuetec2003@ecf54b40-d846-b93a-3702-4cecb57a2510>
2014-11-29 05:40:38 +0000

    Se cambió la ejecución del procedimiento por codigo ado.net en lugar de entity para evitar el conflicto con el cursor

--- AdministracionTicket/Formularios/Form1.Designer.vb
+++ AdministracionTicket/Formularios/Form1.Designer.vb
@@ -25,94 +25,17 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.ATENCIONXOFICINABindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.NUMFILADataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.DEPARTAMENTODataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.MUNICIPIODataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.SUCURSALDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.OFICINADataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.NOMBREEMPLEADODataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.GESTIONDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.GESTIONESATENDIDASDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.PROMEDIOESPERADataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.PROMEDIOATENCIONDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ATENCIONXOFICINABindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
-        Me.DataGridView1.AutoGenerateColumns = False
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NUMFILADataGridViewTextBoxColumn, Me.DEPARTAMENTODataGridViewTextBoxColumn, Me.MUNICIPIODataGridViewTextBoxColumn, Me.SUCURSALDataGridViewTextBoxColumn, Me.OFICINADataGridViewTextBoxColumn, Me.NOMBREEMPLEADODataGridViewTextBoxColumn, Me.GESTIONDataGridViewTextBoxColumn, Me.GESTIONESATENDIDASDataGridViewTextBoxColumn, Me.PROMEDIOESPERADataGridViewTextBoxColumn, Me.PROMEDIOATENCIONDataGridViewTextBoxColumn})
-        Me.DataGridView1.DataSource = Me.ATENCIONXOFICINABindingSource
         Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(716, 392)
         Me.DataGridView1.TabIndex = 0
-        '
-        'ATENCIONXOFICINABindingSource
-        '
-        Me.ATENCIONXOFICINABindingSource.DataSource = GetType(AdministracionTicket.ATENCION_X_OFICINA)
-        '
-        'NUMFILADataGridViewTextBoxColumn
-        '
-        Me.NUMFILADataGridViewTextBoxColumn.DataPropertyName = "NUMFILA"
-        Me.NUMFILADataGridViewTextBoxColumn.HeaderText = "NUMFILA"
-        Me.NUMFILADataGridViewTextBoxColumn.Name = "NUMFILADataGridViewTextBoxColumn"
-        '
-        'DEPARTAMENTODataGridViewTextBoxColumn
-        '
-        Me.DEPARTAMENTODataGridViewTextBoxColumn.DataPropertyName = "DEPARTAMENTO"
-        Me.DEPARTAMENTODataGridViewTextBoxColumn.HeaderText = "DEPARTAMENTO"
-        Me.DEPARTAMENTODataGridViewTextBoxColumn.Name = "DEPARTAMENTODataGridViewTextBoxColumn"
-        '
-        'MUNICIPIODataGridViewTextBoxColumn
-        '
-        Me.MUNICIPIODataGridViewTextBoxColumn.DataPropertyName = "MUNICIPIO"
-        Me.MUNICIPIODataGridViewTextBoxColumn.HeaderText = "MUNICIPIO"
-        Me.MUNICIPIODataGridViewTextBoxColumn.Name = "MUNICIPIODataGridViewTextBoxColumn"
-        '
-        'SUCURSALDataGridViewTextBoxColumn
-        '
-        Me.SUCURSALDataGridViewTextBoxColumn.DataPropertyName = "SUCURSAL"
-        Me.SUCURSALDataGridViewTextBoxColumn.HeaderText = "SUCURSAL"
-        Me.SUCURSALDataGridViewTextBoxColumn.Name = "SUCURSALDataGridViewTextBoxColumn"
-        '
-        'OFICINADataGridViewTextBoxColumn
-        '
-        Me.OFICINADataGridViewTextBoxColumn.DataPropertyName = "OFICINA"
-        Me.OFICINADataGridViewTextBoxColumn.HeaderText = "OFICINA"
-        Me.OFICINADataGridViewTextBoxColumn.Name = "OFICINADataGridViewTextBoxColumn"
-        '
-        'NOMBREEMPLEADODataGridViewTextBoxColumn
-        '
-        Me.NOMBREEMPLEADODataGridViewTextBoxColumn.DataPropertyName = "NOMBRE_EMPLEADO"
-        Me.NOMBREEMPLEADODataGridViewTextBoxColumn.HeaderText = "NOMBRE_EMPLEADO"
-        Me.NOMBREEMPLEADODataGridViewTextBoxColumn.Name = "NOMBREEMPLEADODataGridViewTextBoxColumn"
-        '
-        'GESTIONDataGridViewTextBoxColumn
-        '
-        Me.GESTIONDataGridViewTextBoxColumn.DataPropertyName = "GESTION"
-        Me.GESTIONDataGridViewTextBoxColumn.HeaderText = "GESTION"
-        Me.GESTIONDataGridViewTextBoxColumn.Name = "GESTIONDataGridViewTextBoxColumn"
-        '
-        'GESTIONESATENDIDASDataGridViewTextBoxColumn
-        '
-        Me.GESTIONESATENDIDASDataGridViewTextBoxColumn.DataPropertyName = "GESTIONES_ATENDIDAS"
-        Me.GESTIONESATENDIDASDataGridViewTextBoxColumn.HeaderText = "GESTIONES_ATENDIDAS"
-        Me.GESTIONESATENDIDASDataGridViewTextBoxColumn.Name = "GESTIONESATENDIDASDataGridViewTextBoxColumn"
-        '
-        'PROMEDIOESPERADataGridViewTextBoxColumn
-        '
-        Me.PROMEDIOESPERADataGridViewTextBoxColumn.DataPropertyName = "PROMEDIO_ESPERA"
-        Me.PROMEDIOESPERADataGridViewTextBoxColumn.HeaderText = "PROMEDIO_ESPERA"
-        Me.PROMEDIOESPERADataGridViewTextBoxColumn.Name = "PROMEDIOESPERADataGridViewTextBoxColumn"
-        '
-        'PROMEDIOATENCIONDataGridViewTextBoxColumn
-        '
-        Me.PROMEDIOATENCIONDataGridViewTextBoxColumn.DataPropertyName = "PROMEDIO_ATENCION"
-        Me.PROMEDIOATENCIONDataGridViewTextBoxColumn.HeaderText = "PROMEDIO_ATENCION"
-        Me.PROMEDIOATENCIONDataGridViewTextBoxColumn.Name = "PROMEDIOATENCIONDataGridViewTextBoxColumn"
         '
         'Form1
         '
@@ -128,15 +51,5 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents NUMFILADataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DEPARTAMENTODataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents MUNICIPIODataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents SUCURSALDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents OFICINADataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents NOMBREEMPLEADODataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents GESTIONDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents GESTIONESATENDIDASDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents PROMEDIOESPERADataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents PROMEDIOATENCIONDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents ATENCIONXOFICINABindingSource As System.Windows.Forms.BindingSource
 End Class
--- AdministracionTicket/Reportes/rptAtencionXOficina.vb
+++ AdministracionTicket/Reportes/rptAtencionXOficina.vb
@@ -1,17 +1,67 @@
-﻿Public Class rptAtencionXOficina
-    Public Sub New(Optional idso As Decimal = 0, Optional finicio As String = Nothing, Optional ffin As String = Nothing)
+﻿Imports Oracle.DataAccess.Types
+Imports Oracle.DataAccess.Client
 
+Public Class rptAtencionXOficina
+    Dim cnn As New OracleConnection(My.Settings.MiConexion)
+
+    Public Sub New(Optional idso As Decimal = 0, Optional finicio As String = Nothing, Optional ffin As String = Nothing)
         ' Llamada necesaria para el diseñador.
         InitializeComponent()
 
         ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
-        Dim ctx As New EntidadesVistas(My.Settings.CadenaAPPCAVistas)
-        Dim datos = ctx.SP_RTE_ATENCIONXOFICINA1(idso, finicio, ffin)
-        BindingSource1.DataSource = datos
-
         lblDesde.Text = finicio
         lblHasta.Text = ffin
+
+        Try
+            Using myCMD As New OracleCommand() With {.Connection = cnn, .CommandText = "SP_RTE_ATENCIONXOFICINA", .CommandType = CommandType.StoredProcedure}
+
+                myCMD.Parameters.Add("VIDDETALLE_SUCURSAL_OFICINA", OracleDbType.Int32, 10, Nothing, ParameterDirection.Input).Value = idso
+                myCMD.Parameters.Add("DESDE", OracleDbType.NVarchar2, Nothing, ParameterDirection.Input).Value = finicio
+                myCMD.Parameters.Add("HASTA", OracleDbType.NVarchar2, Nothing, ParameterDirection.Input).Value = ffin
+
+                Dim refCursor As OracleParameter = New OracleParameter With
+                {
+                    .OracleDbType = OracleDbType.RefCursor,
+                    .Direction = ParameterDirection.Output
+                }
+
+                myCMD.Parameters.Add(refCursor)
+                cnn.Open()
+                myCMD.ExecuteNonQuery()
+
+                Dim cursor As OracleRefCursor = DirectCast(refCursor.Value, OracleRefCursor)
+                Dim reader As OracleDataReader = cursor.GetDataReader
+
+                Dim lista As List(Of ATENCION_X_OFICINA) = New List(Of ATENCION_X_OFICINA)
+
+                If reader.HasRows Then
+                    While reader.Read
+                        Dim objeto As New ATENCION_X_OFICINA() With
+                        {
+                            .NUMFILA = reader("NUMFILA").ToString,
+                            .DEPARTAMENTO = reader("DEPARTAMENTO").ToString,
+                            .MUNICIPIO = reader("MUNICIPIO").ToString,
+                            .SUCURSAL = reader("SUCURSAL").ToString,
+                            .OFICINA = reader("OFICINA").ToString,
+                            .NOMBRE_EMPLEADO = reader("NOMBRE_EMPLEADO").ToString,
+                            .GESTION = reader("GESTION").ToString,
+                            .GESTIONES_ATENDIDAS = reader("GESTIONES_ATENDIDAS"),
+                            .PROMEDIO_ESPERA = reader("PROMEDIO_ESPERA"),
+                            .PROMEDIO_ATENCION = reader("PROMEDIO_ATENCION")
+                        }
+
+                        lista.Add(objeto)
+                    End While
+                    reader.Close()
+                    refCursor.Dispose()
+                    BindingSource1.DataSource = lista
+                End If
+            End Using
+        Catch ex As Exception
+        Finally
+            cnn.Close()
+        End Try
     End Sub
 
- 
+
 End Class
